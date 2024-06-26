ff8327c357bc94921b803b12190bc703f549a7c2
MattWoahman <Matt_Homan1@baylor.edu>
2024-06-26 12:20:22 -0500

    Added skeleton for Blast Server section

--- tests/output.c
+++ tests/output.c
@@ -11,9 +11,20 @@ int netcontest(char* domainName){
 	//if (pingtest == failure) { test = 1};
 	return test;
 }
+
 //need to see if I can override the function or make port have a default value (blast's port)
 int portcontest(char* domainName, int port);
 
+int blastServer(int port);
+
+int blastServer(int port){
+	int test = 0;
+	printf("Checking blast service status\n\n");
+	//test port by either nmapping, netstat, ss, whatever can be done / is OS agnostic
+	printf("Checking blast service port\n\n");
+	test = 1;
+	return test;
+}
 int agentservicetest(void);
 
 int main(void) {
@@ -64,8 +75,26 @@ if (input == '1'){
 	printf("%s is your domain name\n", domainName);
 	pingSuccess = netcontest(domainName);
 }
-
-//add ping test
+int blastTrue;
+if (input == '2'){
+	printf("What port is VMWare blast listening on? (default = 22443)\n");
+	fflush(stdin);
+	int port;
+	char buffer[5];
+	char* portInput = fgets(buffer,50,stdin);
+	char portParse[strlen(portInput)-1];
+	for (int i=0; i<(strlen(portInput)); i++){
+		portParse[i] = portInput[i];
+		printf("%c", portParse[i]);
+	}
+	if (strlen(portInput) == 1) {
+		port = 22443;
+	} else {
+		port = atoi(portParse);
+	}
+	printf("Your port is %d!\n", port);
+	blastTrue = blastServer(port);	
+}
 //add trcroute
 if (pingSuccess == 0) {
 	printf("Success! You can reach your domain!\n"); //change this to the ip/domain name
