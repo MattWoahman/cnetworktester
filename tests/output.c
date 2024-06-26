ee9d06e03471281e18837f4fadeabd3ec0ae4fd1
MattWoahman <Matt_Homan1@baylor.edu>
2024-06-26 11:44:27 -0500

    Created tests and made network test selectable

--- tests/output.c
+++ tests/output.c
@@ -1,28 +1,76 @@
 #include <stdio.h>
-int main() {
+#include <string.h>
+#include <stdlib.h>
+
+int netcontest(char* domainName);
+
+int netcontest(char* domainName){
+	printf("Pinging %s ...\n\n", domainName);
+	int test = 0;
+	//if (pingtest == success) { test = 0};
+	//if (pingtest == failure) { test = 1};
+	return test;
+}
+//need to see if I can override the function or make port have a default value (blast's port)
+int portcontest(char* domainName, int port);
+
+int agentservicetest(void);
+
+int main(void) {
+
+FILE* fp;
 
 int pingSuccess = 1;
-char* domainName = "parzival.ctcr.local";
 
-printf("What Horizon server do you want to test?\n");
+
+
+char testType;
+
+int input;
+int count = 0;
+int yn;
+
+printf("VMWare blast connection tester\n\n");
 // add user input and assignment to variable
-printf("What do you want to test?\n");
-//add test selection / tests
+while ( yn != 'y') {
+	
+	printf("What do you want to test?\n");
+	printf("1. Network Connection\n");
+	printf("2. Blast server\n");
+	printf("3. Trace route\n\n");
 
-char* testType = "network connection test";
+	//add test selection / tests
+	fflush(stdin);
+	
+	input = fgetc(stdin);
+	char* tests[3] = {"Network Connection", "Blast server", "Trace route"};
+	int utest = (input - '0');
+	printf("You chose %s... is that correct? (y/n)\n", tests[utest - 1]);
 
 
-printf("You chose %s... is that correct? (y/n)\n", testType);
-// Connection test
-
-printf("Pinging %s ...\n\n", domainName); 
+	fflush(stdin);
+	yn = fgetc(stdin);
+}
+fflush(stdin);
+if (input == '1'){
+	int dn;
+	printf("What is your domain/ip address?\n");
+	char buffer[50];
+	char* domainInput = fgets(buffer,50,stdin);
+	char domainName[strlen(domainInput)-1];
+	for (int i = 0; i < (strlen(domainInput) - 1); i++) {
+		domainName[i] = domainInput[i];
+	}
+	printf("%s is your domain name\n", domainName);
+	pingSuccess = netcontest(domainName);
+}
 
 //add ping test
 //add trcroute
 if (pingSuccess == 0) {
-	printf("Success! You can reach %s!\n", domainName); //change this to the ip/domain name
+	printf("Success! You can reach your domain!\n"); //change this to the ip/domain name
 } else {
-	printf("Failed! You cannot reach %s.\n", domainName);
+	printf("Failed! You cannot reach your domain.\n");
 }
 
 return 0;
